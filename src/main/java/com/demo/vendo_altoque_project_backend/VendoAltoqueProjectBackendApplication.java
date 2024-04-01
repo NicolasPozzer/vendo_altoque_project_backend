f24de2d1cdde6099eaf85e8f84cac68f3f0203cc
NicolasPozzer <pozzernico@gmail.com>
2024-04-01 16:35:22 -0300

    publish in git

---
+++ src/main/java/com/demo/vendo_altoque_project_backend/VendoAltoqueProjectBackendApplication.java
@@ -0,0 +1,13 @@
+package com.demo.vendo_altoque_project_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VendoAltoqueProjectBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VendoAltoqueProjectBackendApplication.class, args);
+	}
+
+}
